079e3c2664de064c818201d871c7e12b6f36a5d7
Miguel Montalvo <miguel.montalvo@barrelny.com>
2023-03-11 21:08:58 -0800

    Example

---
+++ frontend/components/Button.jsx
@@ -0,0 +1,9 @@
+export default function Button (props) {
+  return (
+    <button
+      {...props}
+      disabled={props.disabled}
+      class="button"
+    />
+  )
+}
---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import shopify from 'vite-plugin-shopify'
+import preact from '@preact/preset-vite'
+
+export default defineConfig({
+  plugins: [
+    shopify({
+      themeRoot: 'extensions/shopify-vite'
+    }),
+    preact()
+  ]
+})
