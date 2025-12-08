f841695041205fd6b0a1028259ef8feab4d56a1c
Miguel Montalvo <miguel.montalvo@meetdomaine.com>
2025-12-07 21:02:54 -0600

    Update deps

--- extensions/theme-extension/vite.config.ts
+++ extensions/theme-extension/vite.config.ts
@@ -3,14 +3,6 @@ import shopify from "vite-plugin-shopify";
 
 export default defineConfig({
   plugins: [
-    shopify(),
-  ],
-  server: {
-    cors: {
-      origin: [
-        /^https?:\/\/(?:(?:[^:]+\.)?localhost|127\.0\.0\.1|\[::1\])(?::\d+)?$/,
-        /\.myshopify\.com$/,
-      ],
-    },
-  },
+    shopify({ tunnel: true }),
+  ]
 });
